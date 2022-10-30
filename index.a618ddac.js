172a459c9560841549194a8fedc7e59cd90e0bf8
Ihor-Melnyk <92684157+Ihor-Melnyk@users.noreply.github.com>
2022-10-30 22:47:54 +0000

    Deploying to gh-pages from @ Ihor-Melnyk/FoodSta@278bdcb1d7d189deb5641d4b40acef36c2f9a077 🚀

---
+++ index.a618ddac.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{cssMode:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"},mousewheel:!0,keyboard:!0});
+//# sourceMappingURL=index.a618ddac.js.map
